c57e102644f463e10bed824f437c076402f27877
tyler simmonds <tsimmonds278@gmail.com>
2013-09-15 16:03:29 +0100

    updated TASKS
    Signed-off-by: tyler simmonds <tsimmonds278@gmail.com>

--- servergames/ServerGames/src/com/server/games/main.java
+++ servergames/ServerGames/src/com/server/games/main.java
@@ -1,8 +1,14 @@
 package com.server.games;
 
-public class main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin{
+	
+	public void enable(){
+		
+	}
+	public void disable(){
+		
+	}
 
 }
-this is the main class :D
-
-welldonne :D
