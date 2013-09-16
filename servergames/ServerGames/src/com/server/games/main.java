e1653f6bc5667545f824c80b9efd27280e2eb23a
callum <callumt47@hotmail.co.uk>
2013-09-16 18:17:24 +0100

    Updated Main class so that it works
    Signed-off-by: callum <callumt47@hotmail.co.uk>

--- servergames/ServerGames/src/com/server/games/main.java
+++ servergames/ServerGames/src/com/server/games/main.java
@@ -3,20 +3,16 @@ package com.server.games;
 import java.util.ArrayList;
 
 import org.bukkit.Location;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin{
 	
 	public ArrayList<String> joined = new ArrayList<String>();
 	public Location playerjoinloc;
-	public LevelGeneratorListener lgl;
 	
 	public void onEnable(){
 		LevelGeneratorListener lis = new LevelGeneratorListener(this);
 		
-		PluginManager pm = this.getServer().getPluginManager();
-		lgl = new LevelGeneratorListener(this);
 		
 		this.getCommand("Hex").setExecutor(new LevelGeneratorCommands(this, lis));
 	}
